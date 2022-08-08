65e6e909f9442b093e9a75bd36b122f13d7f1e12
sahinbekir <109946770+sahinbekir@users.noreply.github.com>
2022-08-08 20:52:14 +0300

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'console error: '));
